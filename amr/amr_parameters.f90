a4bc94df3d56200486ab47c00758f1d6da0c0c2c
San Han <sanhan@yonsei.ac.kr>
2020-12-18 05:00:02 +0900

    Fix: stellar_winds_file

--- amr/amr_parameters.f90
+++ amr/amr_parameters.f90
@@ -263,6 +263,7 @@ module amr_parameters
   logical :: drag_part = .false. !activate the friction from stars/DM (HP)
   logical :: holdback = .true. ! Activate strict hold-back method to preserve max physical resolution
   logical :: stellar_winds = .false.
+  character(len=128) :: stellar_winds_file = 'none'
 
   ! Output times
   real(dp),dimension(1:MAXOUT)::aout=1.1       ! Output expansion factors
