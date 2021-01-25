e0439a31ce4d35940baee94fdf9b6864c4e00ce3
San Han <sanhan@yonsei.ac.kr>
2021-01-26 08:28:53 +0900

    Change: disable quenching criterion

--- pm/sink_particle.f90
+++ pm/sink_particle.f90
@@ -293,7 +293,7 @@ subroutine make_sink(ilevel)
   !------------------------------
   ! Determine AGN formation sites
   !------------------------------
-  call quenching(ilevel)
+!  call quenching(ilevel)
 
   !----------------------------
   ! Compute number of new sinks
@@ -340,7 +340,7 @@ subroutine make_sink(ilevel)
            ! Check if gas is Jeans unstable
            if(d    <d_thres)ok(i)=.false.
            ! Quenching criterion
-           if(flag2(ind_cell(i))==1)ok(i)=.false.
+           ! if(flag2(ind_cell(i))==1)ok(i)=.false.
 
            ! ENFORCE MASS CRITERION (MT)
            ! Check if the mass criterion will be met
