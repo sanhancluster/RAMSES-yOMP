09beaad3116bd365ea750ffc97e73c9743afd582
San <sanhan@yonsei.ac.kr>
2022-10-11 16:46:57 +0900

    Fix: +increase threshold

--- hydro/cooling_module.f90
+++ hydro/cooling_module.f90
@@ -1037,7 +1037,7 @@ subroutine solve_cooling(nH,T2,zsolar,fdust,sigma,boost,dt,deltaT2,ncell,ilevel)
            jj1=1+(jj-1)*(dndsize+1)
            jj2=jj1+dndsize
            do ii=jj1,jj2
-              if(rhoZ0(ii)>0.0d0 .and. rhoD00(ii)>1.0d-20)then
+              if(rhoZ0(ii)>0.0d0 .and. rhoD00(ii)>1.0d-15)then
                  wcool=MAX(wcool,oneovertdes(ii)*renorm &
                       &    ,oneovertacc(ii)*(1d0-rhoD00(ii)/rhoZ0(ii))*renorm &
                       &    ,oneovertsha(jj2)*fdust(ind(i),jj2)*renorm &
